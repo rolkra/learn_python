461392d949db0715d299ecdb7ec19bfe8688f875
rolkra <31808268+rolkra@users.noreply.github.com>
2017-12-09 16:19:31 +0100

    Update basics.py

--- basics.py
+++ basics.py
@@ -2,7 +2,13 @@
 # Types
 ################################################################################
 
+str(0)        # convert into string
+int('0')      # convert into integer
+float(0)      # convert into float
+bool(0)       # convert into boolean
 
+# get type (str, int, float, bool, ...)
+type("hello")       
 
 ################################################################################
 # Dictionary (key - value pairs, keys need to be unique)
