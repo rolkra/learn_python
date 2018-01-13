ac6ddcd3aaec7f6c8e4f81c8e22e56043983f5c7
rolkra <31808268+rolkra@users.noreply.github.com>
2018-01-13 17:47:03 +0100

    Create type.py

---
+++ type.py
@@ -0,0 +1,11 @@
+################################################################################
+# Variable types
+################################################################################
+
+str(0)        # convert into string
+int('0')      # convert into integer
+float(0)      # convert into float
+bool(0)       # convert into boolean
+
+# get type (str, int, float, bool, ...)
+type("hello")       
