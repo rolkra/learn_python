64c86adc72d1568de36a4d7e2a68e6ba4285d22d
rolkra <31808268+rolkra@users.noreply.github.com>
2018-01-13 17:47:51 +0100

    Update type.py

--- type.py
+++ type.py
@@ -9,3 +9,55 @@ bool(0)       # convert into boolean
 
 # get type (str, int, float, bool, ...)
 type("hello")       
+
+################################################################################
+# String
+################################################################################
+        
+a = "Hello World"
+a.upper()
+a.lower()
+len(a)
+
+################################################################################
+# Dictionary (key - value pairs, keys need to be unique)
+################################################################################
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe["germany"] = "berlin"
+
+# New element
+europe["hungary"] = "budapest"
+
+# Remove australia
+del(europe["australia"])
+
+# Print europe
+print(europe)
+
+################################################################################
+# Tupels (immutable lists)
+################################################################################
+
+point = (1, 2, 3)
+x, y, z = point
+
+################################################################################
+# List vs numpy array
+################################################################################
+
+# default behavior
+a = [1, 2, 3, 4, 5]
+b = a + a
+print(b)  # result [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+
+# numpy
+import numpy as np
+a = np.array([1,2,3,4,5])
+b = a + a
+print(b) # result [ 2  4  6  8 10]
