db2f5d6b8af273086352d8e212f8aa286741a0b6
rolkra <31808268+rolkra@users.noreply.github.com>
2018-01-15 13:16:57 +0100

    Update learn_readwrite.py

--- learn_readwrite.py
+++ learn_readwrite.py
@@ -6,3 +6,16 @@ file = open('data.txt', 'r')
 print(file.read())
 file.close()
 print(file.closed)   # is file closed?
+
+################################################################################
+# read a text-file using numpy
+################################################################################
+
+# read numerical data
+import numpy as np
+data = np.loadtxt('input.csv', delimiter=',', skip=0)  
+
+# mixed type data
+import numpy as np
+data = np.recfromcsv(file)
+
