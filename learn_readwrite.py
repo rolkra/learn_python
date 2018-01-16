01a64981f5e98a231aa0107abf4d9538cec51a7f
rolkra <31808268+rolkra@users.noreply.github.com>
2018-01-16 10:00:02 +0100

    Update learn_readwrite.py

--- learn_readwrite.py
+++ learn_readwrite.py
@@ -8,7 +8,7 @@ file.close()
 print(file.closed)   # is file closed?
 
 ################################################################################
-# read a text-file using numpy
+# numpy
 ################################################################################
 
 # read numerical data
@@ -19,3 +19,14 @@ data = np.loadtxt('input.csv', delimiter=',', skip=0)
 import numpy as np
 data = np.recfromcsv(file)
 
+################################################################################
+# pandas
+################################################################################
+
+import pandas as pd
+file = 'titanic.csv'
+df = pd.read_csv(file)
+df.head()
+
+# no header, first 5 rows
+data = pd.read_csv(file, nrows=5, header=None)
