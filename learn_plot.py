473e8be88c9807a32050d9587cfdd2d5ae3849ba
rolkra <31808268+rolkra@users.noreply.github.com>
2018-01-23 14:17:46 +0100

    Update learn_plot.py

--- learn_plot.py
+++ learn_plot.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 # create dataframe
 age = [18,18,20,23,21,19,19,19,22,22]
 gender = ["M","M","F","M","M","F","F","F","F","M"]
+weight = [55,65,82,71,69,86,62,75,80,72]
 churn = [True, True, False, False, False, False, True, False, False, True]
 data = pd.DataFrame({'age': age, 'gender': gender, 'churn': churn})
 
@@ -19,3 +20,5 @@ plt.hist(data['age'].dropna())    # na in hist throws an error
 data['gender'].value_counts().plot(kind='barh', title="gender")
 data['churn'].value_counts().plot(kind='barh', title="churn")
 
+# scatterplot
+data.plot(x = 'age', y = 'weight', kind="scatter")
